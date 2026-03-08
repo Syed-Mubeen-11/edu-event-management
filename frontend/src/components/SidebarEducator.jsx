79f77cf8d2727efa174d79df1256b3519923861b
project <khamrunnisashaik76@gmail.com>
2026-03-08 12:17:07 +0530

    Added login page UI

---
+++ frontend/src/components/SidebarEducator.jsx
@@ -0,0 +1,16 @@
+function SidebarEducator() {
+  return (
+    <div>
+      <h2>Educator Panel</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>View Events</li>
+        <li>Upload Materials</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SidebarEducator;
